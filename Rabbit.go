c9a0db20f0082b058a6437b33f2f623afddbe6c6
Dmitry Labutin <dmitry@labutin.net>
2016-01-20 10:26:38 +0300

    first commit

---
+++ Rabbit.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+)
+
+func solve(path string, N int, K int) {
+	if N > 0 {
+		for i := 1; i <= K && i <= N; i++ {
+			newPath := path
+			if path != "" {
+				newPath += "+"
+			}
+			newPath += strconv.Itoa(i)
+			solve(newPath, N - i, K)
+		}
+	} else {
+		fmt.Printf("%s\n", path)
+	}
+}
+
+func main() {
+	flagN := flag.Int("N", 1, "N")
+	flagK := flag.Int("K", 1, "K")
+	flag.Parse()
+
+	solve("", *flagN, *flagK);
+}
